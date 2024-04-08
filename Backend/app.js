2ad80e1e6180d742facedad731a975eaf7306940
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-08 15:19:12 +0530

    models created

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,31 @@
 import express from "express";
 import { dbConnection } from "./databse/dbConnection.js";
+import cors from "cors";
 import {config} from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app=express();
 config({ path: "./config.env" });
+
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      method: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
+  );
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(
+    fileUpload({
+        useTempFiles:true,
+        tempFileDir:"/tmp/",
+    })
+);
 
 dbConnection();
 
