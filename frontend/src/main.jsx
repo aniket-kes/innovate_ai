7b7e68af3e8850ca3a43ce597df4eab67bb628cf
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-10 17:29:16 +0530

    login/signup do ne

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+export const Context=createContext({
+  isAuthenticated:false,
+});
+
+const AppWrapper=()=>{
+  const [isAuthenticated,setIsAuthenticated]=useState(false);
+  const [user,setUser]=useState({});
+
+return(
+  <Context.Provider
+  value={{
+    isAuthenticated,
+    setIsAuthenticated,
+    user,
+    setUser,
+  }}
+  >
+    <App/>
+  </Context.Provider>
+);
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <AppWrapper/>
   </React.StrictMode>,
 )
