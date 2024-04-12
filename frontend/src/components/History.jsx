bdce839a0ecfd32d3f9d0af47acc476a613c9361
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-12 18:50:34 +0530

    stable

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -3,7 +3,7 @@ import "./history.css";
 export default function History({ question, onClick }) {
   return (
     <div className="hi-wrapper" onClick={onClick}>
-      <p>{question.substring(0, 15)}...</p>
+      <p>{question.substring(0, 50)}...</p>
     </div>
   );
 }
