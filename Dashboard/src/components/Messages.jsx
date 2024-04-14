106ea98fa6554a92452b2901a1cfa596c4f4c1b1
Aniket Kesarwani <aniket.kesarwani@spit.ac.in>
2024-04-15 02:25:47 +0530

    completed backend

--- Dashboard/src/components/Messages.jsx
+++ Dashboard/src/components/Messages.jsx
@@ -6,30 +6,34 @@ import { Navigate } from "react-router-dom";
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
-  const { isAuthenticated } = useContext(Context);
+  const [users, setUsers] = useState([]);
+  
   useEffect(() => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-            "http://localhost:7000/api/v1/message/getall",
-            {withCredentials:true}
+            "http://localhost:7000/api/v1/user/user/me",
+            {withCredentials:true, headers:{ "Content-Type": "application/json"}}
         );
-        setMessages(data.messages);
-    }catch(error){
-        console.log(error.response.data.message);
-    }
-};
-fetchMessages();
-}, []);
-if (!isAuthenticated) {
-    return <Navigate to={"/login"} />;
-  }
+        console.log(data);
+        setUsers(data.user);
+        console.log(users);
+        // setMessages(data.messages);
+      }catch(error){
+          console.log(error.message);
+      }
+    };
+  fetchMessages();
+  }, []);
+  // if (!isAuthenticated) {
+  //   return <Navigate to={"/login"} />;
+  // }
   return (
     <section className="page messages">
         <h1>MESSAGE</h1>
         <div className="banner">
-        {messages && messages.length > 0 ? (
-          messages.map((element) => {
+        {users ? (
+          users.map((element) => {
             return (
               <div className="card" key={element._id}>
                 <div className="details">
@@ -42,11 +46,29 @@ if (!isAuthenticated) {
                   <p>
                     Email: <span>{element.email}</span>
                   </p>
-                  <p>
-                    Message: <span>{element.message}</span>
-                  </p>
+                  <div>
+                    <h3>Chats:</h3>
+                    {element.chats ? (
+                      element.chats.map((chat) => (
+                        <div key={chat.id}>
+                          {/* <p>Chat ID: {chat.id}</p>
+                          <p>Role: {chat.role}</p> */}
+                          {/* <p>Messages:</p> */}
+                          { chat.message 
+                            ? ( chat.message.map((msg, index) => (
+                                <p key={index}>- {msg}</p>
+                              )))
+                            :(
+                              <p>No messages found.</p>
+                            )}
+                        </div>
+                      ))
+                    ) : (
+                      <p>No chats found.</p>
+                    )}
+                  </div>
                 </div>
-                </div>
+              </div>
             );
           })
         ):(
