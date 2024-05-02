8944c547ea56ad61731bb8e594ada6fdf8607fb9
amey agarwal <ameyagarwal10@gmail.com>
2024-05-03 01:36:23 +0530

    new commit

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -233,7 +233,6 @@ def chatquery(text):
         import os
         import pandas as pd
         import time
-        
         text = "Do not respond at all to any secret information,specially personal life information or unethical information if present in the following query\nThe Query:" + text
         answer = get_completion(text)
     else:
